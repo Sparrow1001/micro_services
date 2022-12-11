118534149155c0a1029497940974dbc5d657b7ea
Evgeniy Fedorenko <captfl1nt@yandex.ru>
2022-12-11 20:45:18 +0300

    added jwt_token

---
+++ testJWT.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+import pprint
+import json
+
+headers = {
+    'accept': 'application/json',
+}
+
+data = {
+    'grant_type': '',
+    'username': 'alex',
+    'password': '1234',
+    'scope': '',
+    'client_id': '',
+    'client_secret': '',
+}
+
+response = requests.post('http://localhost:8080/v1/login', headers=headers, data=data)
+
+user_access_token = json.loads(response.content.decode())['access_token']
+print(user_access_token)
+res = requests.get('http://localhost:8080/v1/users', headers={'Authorization': f'Bearer {user_access_token}'})
+
+assert res.status_code == 200
+pprint.pprint(
+    json.loads(res.content.decode())
+)
